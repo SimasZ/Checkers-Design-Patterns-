c00d7e16d3be7d7d539da83dd6e7080a2aa0b8b8
Mantas Kleiva <mantas.kleiva@ktu.edu>
2018-10-11 12:06:30 +0300

    Singleton initial creation

--- Checkers/InitialForm.cs
+++ Checkers/InitialForm.cs
@@ -14,18 +14,40 @@ namespace Checkers {
 			InitializeComponent();
 		}
 
-		private void button1_Click(object sender, EventArgs e) {
-			this.Hide();
-			var form2 = new CreateServerForm();
-			form2.Closed += (s, args) => this.Close();
-			form2.Show();
+        private bool checkName(string name)
+        {
+            if (name == "")
+            {
+                label2.Text = "Empty player name field.";
+                return false;
+            }
+            label2.Text = "";
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (checkName(textBox1.Text))
+            {
+                this.Hide();
+                var form2 = new CreateServerForm();
+                form2.Closed += (s, args) => this.Close();
+                form2.Show();
+            }
 		}
 
 		private void button2_Click(object sender, EventArgs e) {
-			this.Hide();
-			var form2 = new JoinServerForm();
-			form2.Closed += (s, args) => this.Close();
-			form2.Show();
+            if (checkName(textBox1.Text))
+            {
+                this.Hide();
+                var form2 = new JoinServerForm();
+                form2.Closed += (s, args) => this.Close();
+                form2.Show();
+            }
 		}
-	}
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
---
+++ Checkers/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Checkers
+{
+    public sealed class Singleton
+    {
+        public string name;
+        public IPAddress ipAdress;
+        private static readonly Singleton INSTANCE = new Singleton();
+
+
+        private Singleton()
+        {
+            this.name = "";
+            this.ipAdress = null;
+        }
+        
+        public static Singleton Instance
+        {
+            get
+            {
+                return INSTANCE;
+            }
+        }
+        
+    }
+}
