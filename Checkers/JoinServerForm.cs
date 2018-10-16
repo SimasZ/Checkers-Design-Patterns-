2117a0e6ecf6ce00f2e0227e5bb2ddb82023b3bf
Simonas <simonas.zabutka@ktu.edu>
2018-10-16 20:07:40 +0300

    +

--- Checkers/JoinServerForm.cs
+++ Checkers/JoinServerForm.cs
@@ -15,29 +15,23 @@ namespace Checkers {
 			InitializeComponent();
 		}
 
-		/*private bool checkIP(string ip) {
+		private bool checkIP(string ip) {
 			IPAddress address;
 			if (IPAddress.TryParse(ip, out address)) {
 				return true;
 			}
 			label2.Text = "Wrong ip address!";
 			return false;
-		}*/
+		}
 
 		private void button1_Click(object sender, EventArgs e) {
-			string ip = "";
-			if (NetConnection.GetIP(ref ip)) {
-				IPAddress.TryParse(ip, out Singleton.Instance.ipAdress);
-				label1.Text = "Your ip: " + ip;
+			if (checkIP(textBox1.Text)) {
+				IPAddress.TryParse(textBox1.Text, out Singleton.Instance.ipAdress);
 
 				//Server ser = new Server();
 				Net.ConnectionBase b = new Net.ConnectionBase(Singleton.Instance.ipAdress, false);
-			}
-			/*if (checkIP(textBox1.Text)) {
-				IPAddress.TryParse(ip, out Singleton.Instance.ipAdress);
 				//Net.Client client = new Net.Client(textBox1.Text);
-				Net.ConnectionBase b = new Net.ConnectionBase(Singleton.Instance.ipAdress, false);
-			}*/
+			}
 		}
 	}
 }
