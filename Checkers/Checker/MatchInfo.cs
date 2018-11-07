fc85fe4d0c0574a087ebfa36af62042def415397
Gatex <Gabrielius.tiknius@gmail.com>
2018-11-07 13:30:59 +0200

    Added Observer pattern, minor changes to GameBoard class

--- Checkers/Checker/MatchInfo.cs
+++ Checkers/Checker/MatchInfo.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Checkers.Net;
 
-namespace Checkers.Checker {
-	abstract class GameBoard : Observer.Observer {
-		public GameBoard(Color firstColor, Color secondColor, NetListener subject)
+namespace Checkers.Checker
+{
+    class MatchInfo : Observer.Observer
+    {
+        public MatchInfo(NetListener subject)
         {
             this.subject = subject;
             this.subject.RegisterObserver(this);
-		}
+        }
 
         public override void Update()
         {
---
+++ Checkers/Net/NetListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Checkers.Checker.Observer;
+
+namespace Checkers.Net
+{
+    class NetListener
+    {
+        List<Observer> observerCollection = new List<Observer>();
+
+        public void EndTurnEvent()
+        {
+            //Some end turn event
+        }
+
+        public void RegisterObserver(Observer observer)
+        {
+            observerCollection.Add(observer);
+        }
+
+        public void UnregisterObserver(Observer observer)
+        {
+            observerCollection.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach(Observer observer in observerCollection)
+            {
+                observer.Update();
+            }
+        }
+    }
+}
