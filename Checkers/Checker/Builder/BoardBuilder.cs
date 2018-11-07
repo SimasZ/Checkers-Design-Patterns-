ff8175dc5e162325132d42daffeae09b6d482c84
Simonas <simonas.zabutka@ktu.edu>
2018-11-07 12:22:20 +0200

    Game board added

---
+++ Checkers/Checker/Builder/BoardBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Checker.Builder {
+	class BoardBuilder {
+		public GameBoard getBoard(int width, int height) {
+			return new GameBoard();
+		}
+
+		public void addFirstColor() {
+
+		}
+
+		public void addSecondColor() {
+
+		}
+	}
+}
