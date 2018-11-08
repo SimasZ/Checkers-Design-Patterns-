bcdf3cedeb29cda61c72dbf1717cc5db5a651401
Redas <rpaliskevicius@gmail.com>
2018-11-08 09:01:09 +0200

    Implement deep cloning for ClassicBoard

--- Checkers/Checker/ClassicBoard.cs
+++ Checkers/Checker/ClassicBoard.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Checkers.Net;
+﻿using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Checkers.Checker.Prototype;
 
 
 namespace Checkers.Checker {
-	class ClassicBoard : GameBoard {
+	class ClassicBoard : GameBoard, IPrototype<ClassicBoard> {
 		private int size;
 
 		public ClassicBoard(Color firstColor, Color secondColor, int _size) : base(firstColor, secondColor){
 			size = _size;
 		}
+
+	    public ClassicBoard Clone()
+	    {
+	        using (var memoryStream = new MemoryStream())
+	        {
+	            var formatter = new BinaryFormatter();
+	            formatter.Serialize(memoryStream, this);
+	            memoryStream.Position = 0;
+
+	            return (ClassicBoard)formatter.Deserialize(memoryStream);
+	        }
+        }
 	}
 }
