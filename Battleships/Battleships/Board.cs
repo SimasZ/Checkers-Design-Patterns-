0fef955a8622f032ab400d4cf87bc61ee4557e9b
Mantas Kleiva <mantas.kleiva@gmail.com>
2018-12-10 20:46:45 +0200

    Added Board, GameInstance, Tile, Unit classes.

---
+++ Battleships/Battleships/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    class Board
+    {
+        private const int BOARDSIZE = 10;
+
+        private Tile[,] tileMap;
+
+        public Board()
+        {
+            tileMap = new Tile[BOARDSIZE, BOARDSIZE];
+        }
+        
+
+    }
+}
---
+++ Battleships/Battleships/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    class Unit
+    {
+        private Tile[] tiles;
+
+        public Unit(Tile[] tiles)
+        {
+            this.tiles = tiles;
+        }
+
+    }
+}
