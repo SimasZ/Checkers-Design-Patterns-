0d813a9933f59035ae9466e3553691afe3f657e7
Mantas Kleiva <mantas.kleiva@gmail.com>
2018-12-16 20:25:37 +0200

    Created Ship class

--- Battleships/Battleships/Board.cs
+++ Battleships/Battleships/Board.cs
@@ -12,11 +12,29 @@ namespace Battleships
 
         private Tile[,] tileMap;
 
+        private List<Ship> ships;
+
         public Board()
         {
             tileMap = new Tile[BOARDSIZE, BOARDSIZE];
+            ships = new List<Ship>();
+            for (int i = 0; i < BOARDSIZE; i++)
+            {
+                for (int j = 0; j < BOARDSIZE; j++)
+                {
+                    tileMap[i, j] = new Tile(i,j);
+                }
+            }
         }
         
+        private void PlaceEntity(int x, int y)
+        {
+            tileMap[x, y].Use();
+        }
 
+        public void AddShip(Ship ship)
+        {
+            ships.Add(ship);
+        }
     }
 }
---
+++ Battleships/Battleships/Ship.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    class Ship : Unit
+    {
+        public string ShipName { get; private set; }
+        public Weapon Weapon { get; private set; }
+
+        public Ship(Tile[] tiles) : base(tiles)
+        {
+
+        }
+
+        public bool IsSunken()
+        {
+            foreach (var tile in this.tiles)
+            {
+                if (!tile.IsHit) // All tiles have to be hit to consider ship sunken
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void SetTilesAsUsed()
+        {
+            foreach (var tile in tiles)
+            {
+                tile.Use();
+            }
+        }
+
+    }
+}
