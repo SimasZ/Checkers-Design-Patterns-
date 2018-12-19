0d02303e84d834e2ff829cfbd66ad64047204294
Gatex <Gabrielius.tiknius@gmail.com>
2018-12-19 20:35:05 +0200

    Reverted

---
+++ Battleships/Battleships/Gameplay/SShip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Battleships.Gameplay
+{
+    public class SShip : Ship
+    {
+        public SShip(List<Tile> tiles) : base(tiles)
+        {
+        }
+
+        public override bool IsMovable()
+        {
+            return false;
+        }
+
+        public override bool IsRotatable()
+        {
+            return false;
+        }
+    }
+}
